a1d85278ca4149ff6ada100ca030398eaec9883f
amcsparron2793 <amcsparron2793@users.noreply.github.com>
2020-09-26 16:52:22 -0400

    first draft - scoreboard.py if statement from check_high_score() - nonworking

--- scoreboard.py
+++ scoreboard.py
@@ -2,7 +2,7 @@ import pygame.font
 from pygame.sprite import Group
 
 from ship import Ship
-
+from os import path
 
 class Scoreboard:
     """ A class to report scoring information. """
@@ -35,6 +35,14 @@ class Scoreboard:
 
     def prep_high_score(self):
         """ Turn the high score into a rendered image. """
+
+        if path.isfile('./Current_HighScore.txt'):
+            with open('./Current_HighScore.txt') as file:
+                self.stats.high_score = file.readline(1)
+            file.close()
+        elif not path.isfile('./Current_HighScore.txt'):
+            pass
+
         high_score = round(self.stats.high_score, -1)
         high_score_str = "{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str, True,
@@ -68,6 +76,11 @@ class Scoreboard:
         """ Check to see if there's a new high score. """
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
+
+            with open('./Current_HighScore.txt', 'w') as file:
+                file.write(self.stats.score)
+            file.close()
+
             self.prep_high_score()
 
     def prep_level(self):
