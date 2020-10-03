8c48cc59739a4d23b1ccc566b1b94f6ace474bf1
amcsparron2793 <amcsparron2793@users.noreply.github.com>
2020-10-03 09:00:33 -0400

    AIPlayer class complete

--- ai_player.py
+++ ai_player.py
@@ -16,3 +16,20 @@ class AIPlayer:
         self.ai_game.stats.game_active = True
 
         # start the main loop for the game
+        while True:
+            # Still call ai_game._check_events(), so we can use keyboard to quit.
+            self.ai_game._check_events()
+
+            if self.ai_game.stats.game_active:
+                self.ai_game.ship.update()
+                self.ai_game._update_bullets()
+                self.ai_game._update_aliens()
+
+            self.ai_game._update_screen()
+
+
+if __name__ == '__main__':
+    ai_game = AlienInvasion
+
+    ai_player = AIPlayer(ai_game)
+    ai_player.run_game()
